6aa61957168f1bd601bffabc98762acd780e4594
damaris-lab <damarisnyawiragacheru@gmail.com>
2024-02-01 10:39:35 -0800

    print.c

--- print.c
+++ print.c
@@ -7,45 +7,93 @@
  */
 int _printf(const char *format, ...)
 {
-	va_list ptr;
+	va_list arg;
 	int count = 0;
 
-	va_start(ptr, format);
+	va_start(arg, format);
 	while (*format)
 	{
-		if (*format != '\0')
+		if (*format == '%')
 		{
 			write(1, format, 1);
-			count++;
+			coun++;
 		}
 		else
 		{
 			format++;
-			if (*format == '\0')
-				break;
-			if (*format == '%')
+			if (*format == 's')
 			{
-				write(1, format, 1);
-					count++;
-			}
+				count = print_str(arg);
 			else if (*format == 'c')
+				count += print_char(arg);
+			else if (*format == 'c')
+				count += print_char(arg);
+			else if (*format == 'i' || *format == 'd')
+				count += print_num(arg);
+			else
 			{
-				char c = va_arg(ptr, int);
-				write(1, &c, 1);
-				count++;
+				write(1, format, -1, 2);
+				count += 2;
 			}
-			else if (*format == 's')
-			{
-				char *str = va_arg(ptr, char *);
-				int length = strlen(str);
-
-				write(1, str, str_len);
-				count++
 			}
 			format++;
 		}
+		va_end(arg);
+		return (count);
 	}
-	va_end(ptr);
-	return(count);
+/**
+ * print_str - prints string character on stdout
+ * @arg:pointer to first arguement
+ * Retur:return string
+ */
+int print_str(va_list arg)
+{
+	char *str;
+	int count;
+
+	count = 0;
+	str = va_arg(arg, char *);
+	write(1, str, strlen(str);
+	count += strlen(str);
+	return (count);
 }
+/**
+ * print_char - prints character on stdout
+ * @arg: pointer to arguements
+ * Return:return c
+ */
+int print_char(va_list arg)
+{
+	int count;
+	char c;
+
+	count = 0;
+	c = va_arg(arg, int);
+	write(1, &c, 1);
+	count += 1;
+	return (count);
+}
+/**
+ * print_num - prints an integer
+ * @arg:pointer to arguement
+ * Return:return int
+ */
+int print_num(va_list arg)
+{
+	int count, num, ret;
+	char array[20];
+
+	count = 0;
+	num = va_arg(arg, int);
+	ret = sprintf(array, "%d", num);
+	if (ret > 0)
+	{
+		write(1, array, strlen(array));
+		count += strlen(array);
+	}
+	return (count);
+}
+}
+
+
 
